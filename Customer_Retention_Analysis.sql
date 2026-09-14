2377bbc9c50bcf9bfdbb3d44bb5e8761e81de09e
mominmohd631-cmyk <mominmohd631@gmail.com>
2026-09-14 13:35:51 +0530

    Delete Customer_Retention_Analysis.sql

--- Customer_Retention_Analysis.sql
+++
@@ -1,8 +0,0 @@
-SELECT
-    Contract,
-    InternetService,
-    COUNT(*) AS churned_customers
-FROM telco_churn
-WHERE Churn = 'Yes'
-GROUP BY Contract, InternetService
-ORDER BY churned_customers DESC;
